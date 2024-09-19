0314a4ad47d66bde8d8cbd1d0e94e5a3e5948442
Daniel Acevedo <107837708+daniace@users.noreply.github.com>
2024-09-19 14:11:59 -0300

    igu

---
+++ practica-igu/MyApp.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+# Solo se necesita una instancia de QApplication por aplicación
+# sys.argv es una lista de argumentos de la línea de comandos
+# Si no sabes como usar sys.argv, QApplication([]) también funciona
+app = QApplication(sys.argv)
+
+# Creamos una instancia de QWidget, que va a ser nuestra ventana
+window = QWidget()
+window.show() # IMPORTANTE, las ventanas están ocultas por defecto
+
+# Ejecutamos el bucle de eventos de la aplicación
+app.exec()
+
+# Tu aplicación no va a cerrarse hasta que el bucle de eventos termine
