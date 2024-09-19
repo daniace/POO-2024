c37058f63d55eb0e363214fca1793c282e78b335
Daniel Acevedo <107837708+daniace@users.noreply.github.com>
2024-09-19 20:32:31 -0300

    Update MyApp.py

--- practica-igu/MyApp.py
+++ practica-igu/MyApp.py
@@ -1,5 +1,5 @@
 import sys
-
+from random import choice
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import (
     QApplication,
@@ -7,15 +7,36 @@ from PyQt6.QtWidgets import (
     QPushButton,
 )
 
+window_titles = [
+    "Mi aplicación",
+    "Sigue siendo mi aplicación",
+    "No me canso de decir que es mi aplicación",
+    "Que paso, ya no es mi aplicación?",
+    "Ya no es mi aplicación",
+    "Es tu aplicación ahora",
+    "Es de quien la quiera",
+    "No es de nadie",
+    "Es de todos",
+    "Es de la humanidad",
+    "Algo malo ocurrió",
+]
+
 # Subclase QMainWindow para customizar la ventana principal
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.n_veces_clickeada = 0
+
         self.setWindowTitle("Mi aplicación")
 
         self.button = QPushButton("Apretame!")
         self.button.clicked.connect(self.el_boton_fue_clickeado)
+
+        self.windowTitleChanged.connect(
+            self.el_titulo_de_la_ventana_cambio
+        )
+
         self.setFixedSize(QSize(400, 300))
         self.setMinimumSize(QSize(200, 150))
         self.setMaximumSize(QSize(600, 450))
@@ -24,11 +45,16 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.button)
 
     def el_boton_fue_clickeado(self):
-        self.button.setText("Ya me apretaste!")
-        self.button.setEnabled(False)
+        print("Clickeado!")
+        nuevo_titulo = choice(window_titles)
+        print("Poniendo el titulo: %s" % nuevo_titulo)
+        self.setWindowTitle(nuevo_titulo)
 
-        # Tambien cambia el titulo de la ventana
-        self.setWindowTitle("Se me jijean los jijonazos")
+    def el_titulo_de_la_ventana_cambio(self, titulo):
+        print("El titulo de la ventana cambio a: %s" % titulo)
+
+        if titulo == "Algo malo ocurrió":
+            self.button.setDisabled(True)
 
 app = QApplication(sys.argv)
 
