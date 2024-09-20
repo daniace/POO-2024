387a5bceac2b898b45ddd1e54b107d388e135e56
Daniel Acevedo <107837708+daniace@users.noreply.github.com>
2024-09-19 21:33:55 -0300

    Añadimos dos widgets al diseño de la ventana, un input para texto y en el "label" se refleja lo que escribimos

--- practica-igu/MyApp.py
+++ practica-igu/MyApp.py
@@ -1,60 +1,35 @@
 import sys
-from random import choice
-from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
-    QPushButton,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
 )
 
-window_titles = [
-    "Mi aplicación",
-    "Sigue siendo mi aplicación",
-    "No me canso de decir que es mi aplicación",
-    "Que paso, ya no es mi aplicación?",
-    "Ya no es mi aplicación",
-    "Es tu aplicación ahora",
-    "Es de quien la quiera",
-    "No es de nadie",
-    "Es de todos",
-    "Es de la humanidad",
-    "Algo malo ocurrió",
-]
 
 # Subclase QMainWindow para customizar la ventana principal
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_veces_clickeada = 0
-
         self.setWindowTitle("Mi aplicación")
 
-        self.button = QPushButton("Apretame!")
-        self.button.clicked.connect(self.el_boton_fue_clickeado)
+        self.label = QLabel()
 
-        self.windowTitleChanged.connect(
-            self.el_titulo_de_la_ventana_cambio
-        )
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
 
-        self.setFixedSize(QSize(400, 300))
-        self.setMinimumSize(QSize(200, 150))
-        self.setMaximumSize(QSize(600, 450))
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
 
-        # Ponemos el widget central de la ventana
-        self.setCentralWidget(self.button)
+        container = QWidget()
+        container.setLayout(layout)
 
-    def el_boton_fue_clickeado(self):
-        print("Clickeado!")
-        nuevo_titulo = choice(window_titles)
-        print("Poniendo el titulo: %s" % nuevo_titulo)
-        self.setWindowTitle(nuevo_titulo)
+        self.setCentralWidget(container)
 
-    def el_titulo_de_la_ventana_cambio(self, titulo):
-        print("El titulo de la ventana cambio a: %s" % titulo)
-
-        if titulo == "Algo malo ocurrió":
-            self.button.setDisabled(True)
 
 app = QApplication(sys.argv)
 
